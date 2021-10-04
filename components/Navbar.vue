21c26283889b9189ab987050f4841156aa73a99e
nomaanshaikh487@gmail.com <nomanshaikh487@gmail.com>
2021-10-04 12:39:22 +0530

    Convert to vue

---
+++ components/Navbar.vue
@@ -0,0 +1,358 @@
+<template>
+<div>
+    <header id="body-header">
+        <nav>
+            <ul class="horizontal-list text-center">
+                <li>
+                    <a href="#home"> Home </a>
+                </li>
+                <li>
+                    <a href="#about"> About1 </a>
+                </li>
+                <li>
+                    <a href="#skills"> Skills </a>
+                </li>
+                <li>
+                    <a href="#Experience"> Experience </a>
+                </li>
+                <li>
+                    <a href="#Education"> Education </a>
+                </li>
+                <li>
+                    <a href="#portfolio"> Portfolio </a>
+                </li>
+                <li>
+                    <a href="#contact"> Contact </a>
+                </li>
+                <li>
+                    <a href="#"> Login </a>
+                </li>
+
+            </ul>
+
+        </nav>
+        <div id="name-social-container">
+            <div class="text-center">
+                <h1 id="my-name">
+                    Nomaan Shaikh
+                </h1>
+            </div>
+            <div>
+                <ul class="horizontal-list text-center social-icons">
+                    <li>
+                        <a href="https://www.linkedin.com/in/nomaan-shaikh-ba5b61110/" target="_blank" rel="noopener">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/Nomaan786" target="_blank" rel="noopener">
+                            <i class="fab fa-github"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/noman.shaikh.376/" target="_blank" rel="noopener">
+                            <i class="fab fa-facebook-square"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/sk_nomaan/" target="_blank" rel="noopener">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://stackoverflow.com/users/16757009/nomaan-shaikh" target="_blank" rel="noopener">
+                            <i class="fab fa-stack-overflow"></i>
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+
+        </div>
+    </header>
+    <main>
+        <section id="about">
+            <div id="my-image">
+                <img src="./image/my-image-google.webp" alt="Nomaan Shaikh">
+            </div>
+            <p id="about-para">
+                A self-motivated IT professional with huge knowledge and proficiency in <span class="text-highlight">
+                    HTML, CSS, & React </span> with mobile responsive web development, With knowledge in <span class="text-highlight"> Node JS </span> as well as strong grip in Content Writer and SEO. Workking Knowledge of <span class="text-highlight"> WordPress and content management systems a plus </span>.
+
+            </p>
+        </section>
+        <!-- Starting from here -->
+
+
+        <section id="skills">
+
+
+        </section>
+
+
+
+        <section id="skills">
+            <h1 class="section-heading mb75px ">
+                <span>
+                    <i class="fas fa-chalkboard-teacher"></i>
+                </span>
+                <span> SKILLS </span>
+            </h1>
+
+            <div class="skills-display">
+
+                <div class="skill-progress">
+                    <div class="fifty-percent mb-blue">
+                        <div class="skill-name">
+                            <span> WordPress  </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="skill-progress">
+                    <div class="eighty-five-percent mb-orange">
+                        <div class="skill-name">
+                            <span> HTML </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="skill-progress">
+                    <div class="eighty-percent mb-light-purple">
+                        <div class="skill-name">
+                            <span> CSS </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="skill-progress">
+                    <div class="fifty-percent mb-teal">
+                        <div class="skill-name">
+                            <span> Java Script </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="skill-progress">
+                    <div class="fifty-percent mb-blue">
+                        <div class="skill-name">
+                            <span> NodeJs </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="skill-progress">
+                    <div class="eighty-percent mb-light-purple">
+                        <div class="skill-name">
+                            <span> MongoDB </span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+
+        <section id="experience">
+
+        </section>
+
+        <section id="education">
+
+        </section>
+        <section id="experience">
+            <h1 class="section-heading" mb75px>
+                <span>
+                    <i class="fa fa-briefcase" aria-hidden="true"></i>
+                </span>
+                <span> Work Experince </span>
+            </h1>
+            <div id="timeline">
+                <div class="timeline-box">
+                    <div class="timeline-container">
+                        <div class="timeline-logo">
+                            <img src="./image/web_forest.webp" alt="Web Forest Logo">
+                        </div>
+                        <h3 class="experience-designation  m0 m-blue"> Web designer </h3>
+                        <h4 class="experience-company-name"> Web Forest </h4>
+                        <h5 class="experience-duration m0"> Aug 2019 - Feb 2020 </h5>
+
+                        <p class="experience-description text-align-justify"> As a web designer, my objective is to make a positive impact on clients, co-workers, and the Internet using my skills and experience to design compelling and attractive websites. <br> Convert PSD to HTML. Convert PSD to Bootstrap.
+                            Checking for errors and de-bugging websites. Client interaction about the project. Making the website responsive.</p>
+                    </div>
+                </div>
+                <div class="timeline-box">
+                    <div class="timeline-container">
+                        <div class="timeline-logo">
+                            <img src="./image/well_academy.webp" alt="Well Academy Logo">
+                        </div>
+                        <h3 class="experience-designation  m0 m-blue"> ASSOCIATE ENGINEER</h3>
+                        <h4 class="experience-company-name"> Well Academy </h4>
+                        <h5 class="experience-duration m0"> Dec 2020- Aug 2021</h5>
+
+                        <p class="experience-description text-align-justify"> Directs software design and development Content Writer, SEO and Digital Marketing Production Management and distribution channel Head </p>
+                    </div>
+                </div>
+                <div class="timeline-box">
+                    <div class="timeline-container">
+                        <div class="timeline-logo">
+                            <img src="./image/surat-smartcitylogo.webp" alt="SMC Logo">
+                        </div>
+                        <h3 class="experience-designation  m0 m-blue"> JR. SOFTWARE ENGINEE</h3>
+                        <h4 class="experience-company-name"> SMC </h4>
+                        <h5 class="experience-duration m0"> AUG 2021 PRESENT </h5>
+
+                        <p class="experience-description text-align-justify"> Developed MERN-stack web applications which processed, analyzed, and rendered data visually. Managed time-sensitive updates, including content changes and database upgrades. </p>
+                    </div>
+                </div>
+                <div class="timeline-box">
+                    <div class="timeline-container">
+                        <div class="timeline-logo">
+                            <img src="./image/Logo (1).webp" alt="ilminggo logo">
+                        </div>
+                        <h3 class="experience-designation  m0 m-blue"> Co-Founder Member </h3>
+                        <h4 class="experience-company-name"> Ilminggo </h4>
+                        <h5 class="experience-duration m0"> Sept-2021 </h5>
+
+                        <p class="experience-description text-align-justify">Recognize Market Opportunities. Forming and orchestrating the team. Lead Product Development. Performing direct Marketing and Promotion launching. Providing Financial Estimates. Create Business Plans and legal Documents Build.
+                            and Lead the Team</p>
+                    </div>
+                </div>
+                <div class="timeline-divider">
+                    <div class="timeline-traveller">
+                        <i class="fas fa-plane"></i>
+
+
+                    </div>
+                </div>
+            </div>
+
+
+        </section>
+
+        <section id="portfolio">
+            <h1 class="section-heading mb75px">
+                <span>
+                    <i class="fas fa-th-list"></i>
+                        </span>
+                <span> Portfolio </span>
+            </h1>
+
+
+            <div id="portfolio-container">
+
+                <div class="portfolio-image-container">
+                    <img src="./image/blockchain.jpg" alt="blockchain">
+                    <div class="portfolio-details">
+                        <p>
+                            Governments and companies generally use tendering to procure goods or services from manufacturing companies or service providers. However, e-tendering being the most used procurement method, there are various security implications present.
+                        </p>
+                    </div>
+
+                </div>
+                <div class="portfolio-image-container">
+                    <img src="./image/mis.webp" alt="MIS">
+                    <div class="portfolio-details">
+                        <p>
+                            A medical information system (MIS) refers to a system designed to manage healthcare data. This includes systems that collect, store, manage and transmit a patient’s electronic medical record (EMR), a hospital’s operational management or a system supporting
+                            healthcare policy decisions.
+                        </p>
+                    </div>
+
+                </div>
+
+                <div class="portfolio-image-container">
+                    <a href="/guess the number/index.html"></a> <img src="./image/guessthenumber.webp" alt="Guess the number">
+
+                    <div class="portfolio-details">
+
+                        <p>
+                            Click here to enter to the game and more details
+                            <a class="btn btn-primary" href="/guess the number/index.html" target="_blank" role="button">Click here</a>
+
+                        </p>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+
+        <section id="contact">
+            <h1 class="section-heading mb50px">
+                <span>
+                    <i class="far fa-address-card"></i>
+                </span>
+                <span> Contact </span>
+            </h1>
+            <div id="contact-container">
+
+                <div id="contact-form-container">
+                    <form id="contact-form">
+                        <input id="input-name" name="name" type="text" placeholder="Your Name">
+                        <input id="input-email" name="input-email" type="text" required placeholder="Your Email">
+                        <textarea id="input-message" name="input-message" rows="2" cols=40 placeholder="Message"></textarea>
+                        <button class="sub-btn" type="submit">SEND MESSAGE</button>
+                    </form>
+                </div>
+                <div id="my-details-container">
+
+                    <h3> Get In touch </h3>
+                    <p> </p>
+
+                    <h3> My Address </h3>
+                    <div class="my-details-info-container">
+                        <i class="fas fa-map-marker-alt"></i>
+                        <span> Adajan Patiya, Surat, Gujarat-India</span>
+                    </div>
+                    <div class="my-details-info-container">
+                        <i class="fas fa-mobile-alt"></i>
+                        <span>+918479775261</span>
+                    </div>
+                    <div class="my-details-info-container">
+                        <i class="far fa-envelope"></i>
+                        <span>nomanshaikh487@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="text-center social-icons">
+
+                <ul class="horizontal-list">
+
+                    <li>
+                        <a href="https://www.linkedin.com/in/nomaan-shaikh-ba5b61110/" target="_blank" rel="noopener">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="https://stackoverflow.com/users/16757009/nomaan-shaikh" target="_blank" rel="noopener">
+                            <i class="fab fa-stack-overflow"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="https://www.instagram.com/sk_nomaan/" target="_blank" rel="noopener">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="https://www.facebook.com/noman.shaikh.376/" target="_blank" rel="noopener">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="https://github.com/Nomaan786" target="_blank" rel="noopener">
+                            <i class="fab fa-github-square"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+
+    </main>
+</div>
+  
+</template>
+<style>
+  @import './navbar.css';
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+<div><head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Author: Nomaan Shaikh,
+    Illustrator: P.Resume, Category: resume, 
+    Length: 10 pages">
+    <meta name="description" content="Peronsal Reesume of My IT Depaartment, Web developer, Web designing and many more">
+    <meta http-equiv="Content-Security-Policy" content="script-src 'none'">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+    <link href="/tailwind.css" rel="stylesheet">
+    <title>Resume</title>
+</head>
+  <Navbar/>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
